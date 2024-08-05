1751231750c416a99562485a30480b06ab91cded
Pao Rodriguez <andrea.rodriguez@facturatech.co>
2024-08-05 17:43:56 -0600

    uso de directivas en about

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -1,13 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { CounterComponent } from "../../../shared/components/counter/counter.component";
+import { CounterComponent } from "@shared/components/counter/counter.component";
 import { WaveAudioComponent } from '../../components/wave-audio/wave-audio.component';
+import { HighlightDirective } from '@shared/directives/highlight.directive';
 
 
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CounterComponent, WaveAudioComponent],
+  imports: [CounterComponent, WaveAudioComponent, HighlightDirective],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
