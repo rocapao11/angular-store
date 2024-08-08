76ee398bd06c84ad29afbcd51ca9a9e7c62575e8
Pao Rodriguez <andrea.rodriguez@facturatech.co>
2024-08-08 14:03:06 -0600

    filtrar por categoria con queryParams|URL params

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,35 +1,61 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, Input, signal, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from '@products/components/product/product.component';
 import { Product } from '@shared/models/product.model';
 import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
+import { Category } from '@shared/models/category.model';
+import { CategoryService } from '@shared/services/category.service';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, ProductComponent],
+  imports: [CommonModule, ProductComponent, RouterLink, RouterLinkActive],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
 
   products = signal<Product[]>([]);
+  categories = signal<Category[]>([]);
   private cartService = inject(CartService);
   private productService = inject(ProductService);
+  private categoryService = inject(CategoryService);
+  @Input() category_id?: string
 
   ngOnInit() {
-    this.productService.getProducts()
+    this.getCategories();
+  }
+
+  ngOnChanges(changes: SimpleChanges){
+    this.getProducts();
+  }
+
+  addToCart(product: Product){
+    this.cartService.addToCart(product)
+  }
+
+  private getProducts(){
+    this.productService.getProducts(this.category_id)
     .subscribe({
       next:(products) => {
         this.products.set(products);
       },
       error: () => {
       }
-    });
-  };
-
-  addToCart(product: Product){
-    this.cartService.addToCart(product)
+    })
   }
+
+  private getCategories(){
+    this.categoryService.getAll()
+    .subscribe({
+      next:(data) => {
+        this.categories.set(data);
+      },
+      error: () => {
+      }
+    })
+  }
+
 }
